38e959278c5b236dd3caf3cbff8c0beb070de371
paoli7612 <paoli7612@gmail.com>
2023-03-08 12:39:35 +0100

    comunity

--- device/template.py
+++ device/template.py
@@ -9,12 +9,12 @@ void setup() {
     WiFi.mode(WIFI_STA);
     WiFi.begin("FASTWEB7612", "modena7612");
     ArduinoOTA.begin();
-    %s
+%s
 }
 
 void loop() {
     ArduinoOTA.handle();
-    %s
+%s
 }
 """
 
