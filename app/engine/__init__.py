3cc7bfcac80a7dabffd4d57a33d985a8bf9ef8f4
paoli7612 <paoli7612@gmail.com>
2023-05-21 00:02:57 +0200

    engine load and finish experiment

--- app/engine/__init__.py
+++ app/engine/__init__.py
@@ -1,9 +1,51 @@
 import time, requests, logging
 from engine import config
 
-logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(levelname)s - %(message)s')
+logging.basicConfig(level=logging.DEBUG, format='%(levelname)s: %(message)s')
 
 class Engine:
+    def __init__(self):
+        self.loop()
+
+    def get_next(self):
+        r = requests.get(config.next())
+        return r.json()
+    
+    def get_finish(self, id):
+        r = requests.get(config.finish(id))
+        return r.json()
+
+    def loop(self):
+        self.running = True
+        while self.running:
+            response = self.get_next()
+            if response['status'] == 'experiment':
+                self.experiment(response)
+            elif response['status'] == 'wait':
+                self.wait(response)
+
+    def experiment(self, response):
+        logging.info(response)
+        time.sleep(2)
+        self.finish(response)
+
+    def finish(self, response):
+        logging.info('__FINISH__')
+        experiment_id = response['experiment_id']
+        print(self.get_finish(experiment_id))
+        time.sleep(1)
+
+    def wait(self, response):
+        logging.debug("Nessun esperimento in coda. Aspetto 2 secondi")
+        time.sleep(2)
+
+
+
+
+
+
+
+a = """
     def __init__(self):
         self.run()
 
@@ -16,11 +58,30 @@ class Engine:
         time.sleep(minutes)
         self.finish(e)
 
+    def start(self):
+        response = requests.get(config.next()).json()
+        if response['status'] == 'experiment':
+            self.experiment(response)
+        elif response['status'] == 'wait':
+            print("Nessun esperimento da runnare. riprovo tra poco")
+            time.sleep(5)
+
     def finish(self, e):
         logging.info('Inizio esperimento. experiment_id: %d' %(e['experiment_id']))
         requests.get(config.finish(e['experiment_id'])).json()
 
     def run(self):
+        e = None
+        while True:
+            s = input("1) next\n2)finish\n3)exit")
+            if s == '1':
+                e = self.start()
+            elif s == '2':
+                e = {'experiment_id': 2}
+                self.finish(e)
+            else:
+                break
+
         self.running = True
         while self.running == True:
             try:
@@ -34,3 +95,4 @@ class Engine:
             except Exception as e:
                 logging.error(e)
                 time.sleep(2)
+"""
