8491e5df5f55a8b79c89fbf7f3b791cd668b5d1a
paoli7612 <paoli7612@gmail.com>
2023-05-30 23:04:42 +0200

    test room

--- app/runEngine.py
+++ app/runEngine.py
@@ -1,4 +1,4 @@
 from engine import Engine
 
 if __name__ == '__main__':
-    Engine('phone', 2)
+    Engine('test', 1)
--- config/esp32.cpp
+++ config/esp32.cpp
@@ -5,10 +5,8 @@
 void setup()
 {
     pinMode(13, OUTPUT);
-    digitalWrite(13, LOW);
 
     Serial.begin(9600);
-
     WiFi.begin("Tomaoliphone", "nonlasoio");
     while (WiFi.status() != WL_CONNECTED)
     {
@@ -21,12 +19,10 @@ void loop()
     ArduinoOTA.handle();
     if (WiFi.status() == WL_CONNECTED)
     {
-        digitalWrite(13, HIGH); 
         Serial.println(WiFi.localIP());
     }
     else
     {
         digitalWrite(13, LOW); 
     }
-    delay(1000);
 }
